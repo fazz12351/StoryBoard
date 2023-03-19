6409753731b4fea91f8bd78f8c8f4213bfc58385
Mohammed Ahmed <mahme029@gold.ac.uk>
2023-03-19 20:50:07 +0000

    Code has been refractored

--- app.js
+++ app.js
@@ -3,36 +3,45 @@ let posts = [];
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+//create a express app
+const app = express();
+
+//we allow ejs files to be executed
+app.set('view engine', 'ejs');
+
+//we allow our bodyparser backage get user data
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//we allow express to use our static pages found in public
+app.use(express.static("public"))
 
 
+
+//this is the inital data sent to the array which displays all messages called posts and push the message in that
 let message1 = {
   "MessageTitle": "Welcome",
   Message: "This is an area for you to express  your emotions, i promise to keep it all a secret. Guess What? I also have secrets where i need to write them down. Why not try it? :)"
 }
-
-const app = express();
-
 posts.push(message1)
 
 
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
+;
 
 
-
+//Initial page
 app.get("/", function (req, res) {
-  res.redirect("/Home")
+  checkMessages();
+  res.render("home", {
+    allMessages: posts
+
+  })
 
 })
 
 
 
-
+//about page
 app.get("/about", function (req, res) {
   res.render("about", {
     allMessages: posts
@@ -40,6 +49,7 @@ app.get("/about", function (req, res) {
   })
 })
 
+//contact page
 app.get("/contact", function (req, res) {
   res.render("contact", {
     allMessages: posts
@@ -47,39 +57,32 @@ app.get("/contact", function (req, res) {
   })
 })
 
-
+//compose page
 app.get("/compose", function (req, res) {
   res.render("compose", {
 
   })
 })
 
+//compose directory. When we submit data from the form, data is passes and retrieved here and we store in into and object which is then pushed into an array
 app.post("/ComposeMessage", function (req, res) {
-  let data={
-    MessageTitle:req.body.MessageTitle,
-    Message:req.body.Message
+  let data = {
+    MessageTitle: req.body.MessageTitle,
+    Message: req.body.Message
 
   }
   posts.push(data)
   res.redirect("/")
 })
 
-
-
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
+
+///////////////////////////////////////////////-----------------------------------------------------/////////////////////////////
 function checkMessages() {
-  if (allMessages.length > 1) {
-    allMessages.splice(0, 1)
+  if (posts.length > 1) {
+    posts.splice(0, 1)
   }
 }
-app.get("/Home", function (req, res) {
-  // checkMessages();
-
-  res.render("home", {
-    allMessages: posts
-
-  })
-})
