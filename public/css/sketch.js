d33cb273d6d3a64eb36c1ce49cd7de4b1f34d50e
Mohammed Ahmed <mahme029@gold.ac.uk>
2023-04-02 18:05:36 +0100

    added a hover underline for the readmore text

--- public/css/sketch.js
+++ public/css/sketch.js
@@ -1,4 +1,4 @@
-const messageLength = 10;
+const messageLength = 20;
 let messageBody = document.querySelectorAll(".post-message")
 let messageTitle = document.querySelectorAll(".post-title")
 
@@ -6,9 +6,8 @@ for (let i = 0; i < messageBody.length; i++) {
     let title = messageTitle[i].innerHTML;
     let message = messageBody[i].innerHTML.split(" ")
     let trunkedMessage = message.slice(0, messageLength)
-    let url = "<a href='/posts/" + title + "'>Readmore</a>"
+    let url = "<a href='/posts/" + title + "'>...Readmore</a>"
     let readMore1 = " <span>" + url + "</span>"
-    console.log(url)
     trunkedMessage = trunkedMessage.join(" ")
     messageBody[i].innerHTML = trunkedMessage
     messageBody[i].innerHTML += readMore1
