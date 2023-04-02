6eb227456f10e46672fe1768b2fbc74436573983
Mohammed Ahmed <mahme029@gold.ac.uk>
2023-04-02 17:55:55 +0100

    Text with has been minised allowing amore cleaner look. A readmore hyperlink has been added which direct the user to the dynamic page which has the information of the clicked post in full

--- public/css/sketch.js
+++ public/css/sketch.js
@@ -1,8 +1,17 @@
+const messageLength = 10;
+let messageBody = document.querySelectorAll(".post-message")
+let messageTitle = document.querySelectorAll(".post-title")
 
-const messageLength=100;
-let messageBody=document.querySelectorAll(".post-message")
+for (let i = 0; i < messageBody.length; i++) {
+    let title = messageTitle[i].innerHTML;
+    let message = messageBody[i].innerHTML.split(" ")
+    let trunkedMessage = message.slice(0, messageLength)
+    let url = "<a href='/posts/" + title + "'>Readmore</a>"
+    let readMore1 = " <span>" + url + "</span>"
+    console.log(url)
+    trunkedMessage = trunkedMessage.join(" ")
+    messageBody[i].innerHTML = trunkedMessage
+    messageBody[i].innerHTML += readMore1
 
-for(let i=0;i<messageBody.length;i++){
-    let currentString=messageBody[i].innerHTML
-    console.log(currentString.length)
+
 }
